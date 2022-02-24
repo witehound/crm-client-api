78ea7d61fda189be115835f747749651a0e46c34
InventedMost <netvid00@gmail.com>
2022-02-24 20:09:14 +0100

    dependencies added, with a basic server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("got it")
+const express = require("express")
+const app = express()
+const bodyparser = require('body-parser')
+const cors = require("cors")
+const helmet = require("helmet")
+
+
+const port = process.env.PORT || 3001
+
+app.use( "/", (req, res) => {
+    res.json({message: "hi there you"})
+}) 
+
+app.listen (port,() => {
+   console.log(`API is ready on https://localhost:${port}`)
+})
