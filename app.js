1ac8a65c06dc040b86af018ff5457be62b6334bf
InventedMost <netvid00@gmail.com>
2022-02-26 12:42:45 +0100

    mongodb connection done

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const bodyparser = require('body-parser')
 const cors = require("cors")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const mongoose = require("mongoose")
+
+//my port
+const port = process.env.PORT || 3001
 
 //handle error
 const handleError = require("./src/utils/errorhandler")
@@ -16,12 +21,29 @@ app.use(cors())
 
 app.use(morgan("tiny"))
 
+//mongodb connection setup
+mongoose.connect(process.env.MONGO_URL, 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+if (process.env.NODE_ENV !== "production"){
+    const mDb = mongoose.connection
+    mDb.on("open", () => {
+    console.log("mongodb is connected")
+   })
+   mDb.on("erroe", (error) => {
+    console.log(error)
+   })
+   app.use(morgan("tiny"))
+}
+
+
 //set body parser to req json
 app.use(bodyparser.urlencoded({extended: true}))
 app.use(bodyparser.json())
 
-//my port
-const port = process.env.PORT || 3001
+
 
 //load routers
 const userRouter = require("./src/routers/user.router")
